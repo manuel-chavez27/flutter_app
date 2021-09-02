e59a74b184e286be33b99a7614e97e0a7b2f91e9
manuel-lemon <manuel.chavez@lemonwebdesign.mx>
2021-09-02 17:12:33 -0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   static final String title = 'Localization';
-
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
         create: (context) => LocaleProvider(),
